6cf13b777ee3f50bf55d0adf69f67a3ea63bc7ea
Weeraphat-T <s6503051624068@email.kmutnb.ac.th>
2025-03-03 23:59:44 +0700

    lol
    lol

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -25,10 +25,10 @@ class Settings_Screen extends StatelessWidget {
             Icons.arrow_back,
             size: 40.0,
             color: Colors.white,
-            ),
           ),
-          title: Column(
-            children: [
+        ),
+        title: Column(
+          children: [
             const SizedBox(
               height: 1.5,
             ),
@@ -39,9 +39,126 @@ class Settings_Screen extends StatelessWidget {
                   fontSize: 25,
                   fontWeight: FontWeight.w500),
             ),
-            ],
-          ),
+          ],
         ),
+      ),
+      body: Column(
+        children: [
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(41, 41, 41, 1),
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                  child: SizedBox(
+                    width: 700,
+                    height: 300,
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Container(
+                              height: 120,
+                              width: 120,
+                              decoration: BoxDecoration(
+                                color: Colors.transparent,
+                                shape: BoxShape.circle,
+                              ),
+                              padding: const EdgeInsets.all(8.0),
+                              child: CircleAvatar(
+                                backgroundImage:
+                                    AssetImage("assets/MyProfile.jpg"),
+                              ),
+                            ),
+                            Text(
+                              'Weeraphat_T',
+                              style: GoogleFonts.roboto(
+                                color: Colors.white,
+                                fontSize: 30,
+                                fontWeight: FontWeight.bold,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              'Friend Code',
+                              style: GoogleFonts.roboto(
+                                color: Colors.grey,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                              textAlign: TextAlign.start,
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              'SW - 5555 - 5555 - 5555',
+                              style: GoogleFonts.roboto(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                              textAlign: TextAlign.start,
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            GestureDetector(
+                              onTap: () {},
+                              child: Container(
+                                width: 70,
+                                height: 60,
+                                decoration: BoxDecoration(
+                                  color: Color.fromRGBO(88, 88, 88, 1),
+                                  borderRadius: BorderRadius.circular(30.0),
+                                ),
+                                child: Icon(
+                                  Icons.copy,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            GestureDetector(
+                              onTap: () {},
+                              child: Container(
+                                width: 70,
+                                height: 60,
+                                decoration: BoxDecoration(
+                                  color: Color.fromRGBO(88, 88, 88, 1),
+                                  borderRadius: BorderRadius.circular(30.0),
+                                ),
+                                child: Icon(
+                                  Icons.more_vert,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
